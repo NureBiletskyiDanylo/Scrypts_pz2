806d5ff80f24816361515f8a62bf3fbcd702ff8c
Kirikosha <kirikoshamain@gmail.com>
2024-04-16 16:20:54 +0200

    practical task version 1

---
+++ counter.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION['counted']))
+{
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    $uri = $_SERVER['REQUEST_URI'];
+    $user = $_SERVER['PHP_AUTH_USER'];
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $ref = $_SERVER['HTTP_REFERER'];
+    $dtune = date('r');
+    if (!$ref)
+    {
+        $ref = "HI";
+    }
+    if (!$user)
+    {
+        $user = "HI";
+    }
+
+    $entry_line = "$dtime-IP: $ip | Agent: $agent | URL: $uri | Referer: $ref | Username: $user n";
+    $fp = fopen("logs.txt", "a");
+    fputs($fp, $entry_line);
+    fclose($fp);
+    $_SESSION['counted'];
+}
